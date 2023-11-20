7f48277eecc94b95c97a272bce405a5e42c0e9bf
CRAIG MUTEGI MUTUGI <craigmutegi@gmail.com>
2023-11-21 00:09:29 +0300

    update deployment file

--- scripts/run.js
+++ scripts/run.js
@@ -1,16 +1,21 @@
-const hre = require("hardhat");
+const {ethers} = require("hardhat");
 
-async function main() {
-  console.log("Deploying Contract...");
-  const elon = await hre.ethers.deployContract("ElonNFT");
-  console.log("Contract deployed to address:", elon.target);
+async function main(){
+
+  const deployNFTContract = await ethers.deployContract("ElonNFT");
+
+  const elon = await deployNFTContract.waitForDeployment();
+
+  console.log("Elon contract address is:", await elon.getAddress());
+
+  console.log("Minting NFT...");
+  let txn = await elon.mintNFT();
+  await txn.wait();
+  console.log("Yayy.. NFT minted successfully!!!");
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+main().then(() => process.exit(0))
+.catch((err) => {
+  console.error(err);
+  process.exit(1);
+})

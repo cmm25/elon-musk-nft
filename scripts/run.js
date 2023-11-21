0ac3a67eb0fec69a647360712fd0fbd746161c2b
CRAIG MUTEGI MUTUGI <craigmutegi@gmail.com>
2023-11-21 22:57:38 +0300

    new code to configure mumbai network in run.js

--- scripts/run.js
+++ scripts/run.js
@@ -1,21 +1,20 @@
-const {ethers} = require("hardhat");
-
-async function main(){
-
-  const deployNFTContract = await ethers.deployContract("ElonNFT");
-
-  const elon = await deployNFTContract.waitForDeployment();
-
-  console.log("Elon contract address is:", await elon.getAddress());
-
+async function main() {
+  const ElonNFT = await ethers.getContractFactory("ElonNFT");
+  console.log("Deploying Contract...");
+  const elon = await ElonNFT.deploy();
+  const txHash = elon.deployTransaction.hash;
+  const txReceipt = await ethers.provider.waitForTransaction(txHash);
+  console.log("Contract deployed to address:", txReceipt.contractAddress);
   console.log("Minting NFT...");
   let txn = await elon.mintNFT();
   await txn.wait();
-  console.log("Yayy.. NFT minted successfully!!!");
 }
 
-main().then(() => process.exit(0))
-.catch((err) => {
-  console.error(err);
-  process.exit(1);
-})
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });

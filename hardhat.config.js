ac8f96c722447940e3e08f5a9bab8152f58408c6
CRAIG MUTEGI MUTUGI <craigmutegi@gmail.com>
2023-11-21 22:54:38 +0300

    config hardhut.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
-require("@nomicfoundation/hardhat-toolbox");
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const { API_URL_KEY, PRIVATE_KEY } = process.env;
+
 module.exports = {
   solidity: "0.8.20",
+  defaultNetwork: "mumbai",
+  networks: {
+    hardhat: {},
+    mumbai: {
+      url: API_URL_KEY,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
+
